ae5841c423da58802f8c3b665a715a9aba4493b9
Falekin7 <falekinldg@gmail.com>
2024-11-06 13:06:23 -0300

    install dependences

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
 import mainRouter from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+app.use(morgan('dev'));
 app.use('/', mainRouter);
 
 export default app;
